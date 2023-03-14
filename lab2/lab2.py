2bce64ffe5cd0d438827e116ffe08704397f2019
andrei45635 <iacob.andrei34@gmail.com>
2023-03-14 09:41:55 +0200

    Final Commit lab2

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,6 +2,7 @@ import os
 import networkx as nx
 import matplotlib.pyplot as plt
 import warnings
+from copy import deepcopy
 
 
 def readNetAdjList(fileName):
@@ -29,21 +30,27 @@ def readNet(fileName):
     return gr
 
 
-def modularity(gr, cmty):
-    k_i = 0
+def getDegreeNodes(g):
+    sum_dg = 0
+    for node in g:
+        sum_dg += g.degree(node)
+    return sum_dg
+
+
+def modularity(gr):
+    q = 0
     edge_qs = list(nx.edge_betweenness_centrality(gr).items())
-    m = gr.number_of_edges()
-    m_comm = gr.subgraph(cmty).number_of_edges()
-    for node in cmty:
-        k_i += gr.degree(node)
-    q = ((m_comm / m) - ((k_i / 2 * m) ** 2))
+    for cmt in nx.connected_components(g):
+        subg = g.subgraph(cmt)
+        q += ((subg.number_of_edges()/g.number_of_edges()) - (getDegreeNodes(subg)/(2 * g.number_of_edges())) ** 2)
     return max(edge_qs, key=lambda item: item[1])[0]
 
 
 def colorCommunities(gr):
     community = [1] * g.number_of_nodes()
     color = 0
-    for comm in nx.connected_components(gr):
+    components = nx.connected_components(gr)
+    for comm in components:
         color += 1
         for node in comm:
             community[node] = color
@@ -52,15 +59,15 @@ def colorCommunities(gr):
 
 def greedyCommunitiesDetection(gr, comms):
     while len(list(nx.connected_components(gr))) < comms:
-        sink, source = modularity(gr, [])
+        sink, source = modularity(gr)
         gr.remove_edge(sink, source)
 
     color_communities = colorCommunities(gr)
     return color_communities
 
 
-def plotNetwork(gr, cms):
-    if not cms:
+def plotNetwork(gr, cms=None):
+    if cms is None:
         cms = [0 for _ in range(0, gr.number_of_edges())]
     pos = nx.spring_layout(gr, k=0.3, iterations=20)
     nx.draw_networkx(gr, pos, node_size=600, node_color=cms, arrows=False, with_labels=True)
@@ -69,11 +76,11 @@ def plotNetwork(gr, cms):
 
 if __name__ == '__main__':
     crtDir = os.getcwd()
-    filePath = os.path.join(crtDir, 'data', 'footballEdges.txt')
+    filePath = os.path.join(crtDir, 'data', 'kcomplete2.txt')
     nt = readNet(filePath)
     adj_mat = nx.adjacency_matrix(nt)
     g = nx.Graph(adj_mat)
-    coms = greedyCommunitiesDetection(g, 2)
+    coms = greedyCommunitiesDetection(deepcopy(g), 2)
     print(coms)
     plotNetwork(g, coms)
 
