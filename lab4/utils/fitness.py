2edfad9aa773b3d6fccaea76da528859e4d0a46e
Andrei <andrei.iacob@stud.ubbcluj.ro>
2023-03-25 14:08:40 +0200

    4th Commit

--- lab4/utils/fitness.py
+++ lab4/utils/fitness.py
@@ -2,4 +2,5 @@ def fitness(costs, path):
     fit = 0
     for i in range(0, len(path)):
         fit += costs[path[i - 1] - 1][path[i] - 1]
+    fit += costs[path[0] - 1][path[len(path) - 1] - 1]
     return fit
